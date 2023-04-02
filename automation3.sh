3cbb381044041037739b293f058e68c411caab54
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-04-02 08:01:32 +0530

    Update automation3.sh

--- automation3.sh
+++ automation3.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 array=(helloservice hiservice nameservice managerservice teamservice)
 for line in "${array[@]}"
 do
-echo $line
+    COUNT=`ps -ef | grep helloservice  | grep -v grep | wc -l`.  RUNNING , 3
+    MAX=2
+    echo $line
+    echo $COUNT
+        if [ $COUNT -gt $MAX ]
+    then
+       echo $line
+        PROCS=`ps -ef| grep $line | grep -v grep | awk '{print $2, $11, $12, $13}' | sort -k 4`
+        JAR=`echo "${PROCS}" | awk -F"-Djar_name=| " '{print $5}'`
+        echo $JAR
+        JAR_RUN=`echo $JAR | sed 's/ /,/g'`
+        echo $JAR_RUN
+       cd /apps/nnos/vzomega/scripts
+        ./mail.sh $line $JAR_RUN $COUNT
+    fi
 done
-
