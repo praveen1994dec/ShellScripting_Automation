34e15dc191f0710b32b244d895e56499001717ea
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-04-02 08:29:55 +0530

    Update automation4.sh

--- automation4.sh
+++ automation4.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /opt/logs/ 
-find . -type f -mtime -2 -exec cp '{}' opt/logs/Log_Backup \;
-cd /opt/logs/
-mkdir –p /opt/logs/Log_Backup
+find . -type f -name '*log*' -exec cp '{}' /opt/logs/Log_Backup \; 
+cd /opt/logs/Log_Backup 
 find /opt/logs/Log_Backup -type f -name '*log*' > include-file
 tar -cvf $(hostname)_$(date +%Y-%m-%d).tar.gz -T include-file
 exit
