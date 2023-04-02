d4cab3ffaae09a8b484a6b084586626181bbcd5a
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-04-02 08:12:54 +0530

    Update automation4.sh

--- automation4.sh
+++ automation4.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-cd /Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/ShellScripting_Automation
-
-find . -type f -mtime -2 -exec cp '{}' /Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/ShellScripting_Automation/Log_Backup \;
-
-cd /Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/ShellScripting_Automation/Log_Backup
-
-find . /Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/ShellScripting_Automation/Log_Backup -type f -name '*log*' > include-file
-
+cd /opt/logs/ 
+find . -type f -mtime -2 -exec cp '{}' opt/logs/Log_Backup \;
+cd /opt/logs/
+mkdir –p /opt/logs/Log_Backup
+find /opt/logs/Log_Backup -type f -name '*log*' > include-file
 tar -cvf $(hostname)_$(date +%Y-%m-%d).tar.gz -T include-file
-
-find . -type f -name '*log*' -exec rm -rf {} \;
-find . -type f -mtime +3 -name '*.tar.gz' -exec rm '{}' \;
 exit
-
-
