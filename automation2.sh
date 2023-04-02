e7a71d495fba0ddd8574bad6e4bcd427e6626cda
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-04-02 07:50:51 +0530

    Update automation2.sh

--- automation2.sh
+++ automation2.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
 version="1.2.3.4"
-
 for line in $(cat jiraid.txt)
-
 do
-
- curl -X PUT -u "singampallipraveenkumar@gmail.com:XkO2YJUvaof4fsvLAqtM63EA" --data '{"update":{"labels":[{"add":"DEMO_NEW1"}]}}' -H "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/SA-1
-
- #curl -X PUT -u "singampallipraveenkumar@gmail.com:XkO2YJUvaof4fsvLAqtM63EA" --data {"update":{"comment":[{"add":{"body":"TEST"}}]}}' --header "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/$line
-
+curl -X PUT -u "your_atlassian_jira_username:your_atlassian_jira_token" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/SA-1/comment
 done 
-
