34d9f1d783a1b532f1796b3524208ca8c3297116
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-04-02 07:35:13 +0530

    Update automation1.sh

--- automation1.sh
+++ automation1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CICD=true
-WORKSPACE=/Users/praveensingampalli/Documents/BOOTCAMP2_FINAL/ShellScripting_Automation
+WORKSPACE=/opt/
 JOB_BASE_NAME=Test_demo
 BUILD_NUMBER=10
 if [ $CICD = true ]
@@ -11,10 +11,7 @@ REPORTNAME=${JOB_BASE_NAME}_${BUILD_NUMBER}.Test_demo_10
 echo "CICD Check starting"
   if [ -f "$file" ]; then
         echo "testReport file found sending to artifactory"
-#       curl -H X-JFrog-Art-Api:Token -T $file https://oneartifactorycloud/artifactory/CICD/Reports/$REPORTNAME.html
-#       curl -H X-JFrog-Art-Api:Token -T $file https://oneartifactorycloud/artifactory/CICD/Reports/$REPORTNAME.html
-#        curl -X PUT -u "xyz@gmail.com:Token"--data '{"update":{"labels":[{"add":"DEMO"}]}}' --header "Content-Type: application/json" "Authorization: Basic XkO2YJUvaof4fsvLAqtM63EA"   https://singam.atlassian.net/rest/api/3/issue/SA-1
-
+       curl -H X-JFrog-Art-Api:Token -T $file https://oneartifactorycloud/artifactory/CICD/Reports/$REPORTNAME.html
   else
    echo "testReport file found sending to artifactory"
   fi
