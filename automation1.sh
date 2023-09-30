0ec64a7914992e05e511a975596acf751d1674cc
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-09-30 18:16:49 +0530

    Update automation1.sh

--- automation1.sh
+++ automation1.sh
@@ -11,7 +11,7 @@ REPORTNAME=${JOB_BASE_NAME}_${BUILD_NUMBER}.Test_demo_10
 echo "CICD Check starting"
   if [ -f "$file" ]; then
         echo "testReport file found sending to artifactory"
-       curl -H X-JFrog-Art-Api:Token -T $file https://oneartifactorycloud/artifactory/CICD/Reports/$REPORTNAME.html
+        #curl -H X-JFrog-Art-Api:Token -T $file https://oneartifactorycloud/artifactory/CICD/Reports/$REPORTNAME.html
    else
    echo "testReport file not found"
   fi
