559441e8c91b29a1c22f59878fa8ffe7b3529b2f
Praveen Singampalli <praveensingampalli1994@gmail.com>
2023-12-03 10:19:10 +0530

    Update automation2.sh

--- automation2.sh
+++ automation2.sh
@@ -2,5 +2,5 @@
 version="1.2.3.4"
 for line in $(cat jiraid.txt)
 do
-curl -X PUT -u "your_atlassian_jira_username:your_atlassian_jira_token" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/SA-1
+curl -X PUT -u "your_atlassian_jira_username:your_atlassian_jira_token" --data '{"update":{"labels":[{"add":"DEMO_NEW"}]}}' -H "Content-Type: application/json" https://singam.atlassian.net/rest/api/3/issue/$line
 done 
